9c35ad6b23899021d1023ee421906a2bb9d9c42d
itsace06 <159113768+itsace06@users.noreply.github.com>
2025-12-16 10:23:33 +0800

    Add files via upload

---
+++ LAB7/src/main/java/com/inovero/lab_7/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.inovero.lab_7;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// JpaRepository<EntityClass, IdDataType>
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // Spring Data can automatically generate a query from the method name:
+    List<Product> findByNameContaining(String name);
+}
